41e066b3c11332cdb88fd0faeb94b25cabb7b77a
sangmin park <psm7177@naver.com>
2023-02-21 23:03:16 +0900

    Refactoring style

--- src/component/Input.tsx
+++ src/component/Input.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Input = styled.input`
   width: 220px;
-  height: 18px;
+  height: 32px;
   border: 1px solid #ccc;
   padding: 10px;
   border-radius: 5px;
